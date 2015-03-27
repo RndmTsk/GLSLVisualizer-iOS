d8a55f9ef1c7cbc3d9f2a03e7f7e5002d5db7e3c
RndmTsk <tlatanville@gmail.com>
2015-03-27 17:16:19 -0400

    Initial Commit

---
+++ GLSLVisualizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLSLVisualizer
+//
+//  Created by Terry Latanville on 2015-03-27.
+//  Copyright (c) 2015 Rndm.Studio(). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
